5b6842db8ef9a9fd6725575e3626b00d28c9ef4b
prembf <100865211+prembf@users.noreply.github.com>
2022-04-22 01:52:24 +0700

    Update 22april.sql

--- 22april.sql
+++ 22april.sql
@@ -1,4 +1,6 @@
 --Мяу
-
-SELECT companie.str, SUBSTRING(users.gmail, (LOCATE('@', users.yandex))+1) AS `domen`, COUNT(*) AS `count user` FROM companie 
-JOIN users ON companie.id = users.companie GROUP BY `Domen`, companie.str ORDER BY companie.nazwanie
+--Мяу
+SELECT companie.str, SUBSTRING(users.gmail, (LOCATE('@', users.yandex))+1) AS `domen`, COUNT(*) AS `count user` FROM companie --Мяу
+JOIN users ON companie.id = users.companie GROUP BY `Domen`, companie.str ORDER BY companie.nazwanie--Мяу
+--Мяу
+--Мяу
